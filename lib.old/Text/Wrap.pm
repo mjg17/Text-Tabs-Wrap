81b858206128d45d979b6ab6c00ad8afb8a6b275
David Muir Sharnoff <muir@idiom.org>
2013-09-10 18:47:14 -0700

    bump version

--- lib.old/Text/Wrap.pm
+++ lib.old/Text/Wrap.pm
@@ -7,7 +7,7 @@ require Exporter;
 @EXPORT = qw(wrap fill);
 @EXPORT_OK = qw($columns $break $huge);
 
-$VERSION = 2013.0523;
+$VERSION = 2013.0910;
 $SUBVERSION = 'old';
 
 use vars qw($VERSION $SUBVERSION $columns $debug $break $huge $unexpand $tabstop $separator $separator2);
